3f786e13bcea1c8dc16745d7edf7985241f16a01
Ritam Majumdar <149047274+ritam1105@users.noreply.github.com>
2026-04-26 02:04:41 +0530

    commit

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -2,32 +2,35 @@ import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { FaTimes } from 'react-icons/fa'
 import Auth from '../pages/Auth'
+
 function AuthModel({ onClose }) {
-    const { userData } = useSelector((state) => state.user)
-    useEffect(() => {
-        if (userData) {
-            onClose()
-        }
-    }, [userData, onClose])
+  const { userData } = useSelector((state) => state.user)
 
-    return (
-        <div className="fixed inset-0 z-999 flex items-center justify-center bg-black/10 backdrop-blur-sm px-4">
+  useEffect(() => {
+    if (userData) {
+      onClose()
+    }
+  }, [userData, onClose])
 
-            <div className="relative w-full max-w-md">
+  return (
+    <div className="fixed inset-0 z-999 flex items-center justify-center bg-black/10 backdrop-blur-sm px-4">
 
-                <button
-                    onClick={onClose}
-                    className="absolute top-8 right-5 text-gray-800 hover:text-black text-xl"
-                >
-                    <FaTimes size={18} />
-                </button>
+      <div className="relative w-full max-w-md">
 
-                <Auth isModel={true} />
+        <button
+          onClick={onClose}
+          className="absolute top-8 right-5 z-1000 text-gray-800 hover:text-black text-xl"
+        >
+          <FaTimes size={18} />
+        </button>
 
-            </div>
-
+        <div className="relative z-10">
+          <Auth isModel={true} />
         </div>
-    )
+
+      </div>
+    </div>
+  )
 }
 
 export default AuthModel
