ed7e36a42bc7e707130c91871f94db7206f7eee1
Ritam Majumdar <149047274+ritam1105@users.noreply.github.com>
2026-06-12 17:47:00 +0530

    Update ServerUrl to production URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory';
 import InterviewReport from './pages/InterviewReport';
 
 
-export const ServerUrl="http://localhost:8000"
+export const ServerUrl="https://ai-interview-agent-six-rho.vercel.app/"
 
 function App() {
   const dispatch=useDispatch()
